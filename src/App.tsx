ae9cd3c5f05a4bb0961d538174a68c5beeace417
AlainCommits <alain.lilic1@gmail.com>
2025-02-13 17:41:36 +0100

    client setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello Tailwind + Vite
+      </h1>
+    </div>
+  )
+}
+
+export default App
